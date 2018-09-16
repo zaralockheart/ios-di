f27694c82b9f290a4ef0281e4faa5e9354092168
yusuf <zaralockheart@gmail.com>
2018-09-16 12:57:47 +0800

    this is the proper way for di

--- Dependency Injection Explained/AppDelegate.swift
+++ Dependency Injection Explained/AppDelegate.swift
@@ -16,12 +16,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 
-        if let viewController = window?.rootViewController as! ViewController? {
-
-            let someService = SomeService.shared
-            viewController.someService = someService
-        }
+        let someService = SomeService.shared
         
+//        if let viewController = window?.rootViewController as! ViewController? {
+//
+//
+//            viewController.someService = someService
+//        }
+
+        let viewController = ViewController(someService: someService)
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = viewController
+        window.makeKeyAndVisible()
+        
+        self.window = window
         
         return true
     }
--- Dependency Injection Explained/ViewController.swift
+++ Dependency Injection Explained/ViewController.swift
@@ -10,14 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var someService: SomeService?
-
+    var someService: SomeService
+    
+    init(someService: SomeService){
+        self.someService = someService
+        super.init(nibName: nil, bundle: nil)
+        
+        view.backgroundColor = .red
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
 //        SomeService.shared.doStuff()
-
-        someService?.doStuff()
+        
+        someService.doStuff()
     }
 
 
